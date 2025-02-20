4e856d29520b239f606c44f2a32ff08facff16e9
Mahmoud Beeko <mahmoud.m.bakir@gmail.com>
2025-02-20 14:59:34 +0200

    modified API folder in core

---
+++ lib/core/api/endpoints.dart
@@ -0,0 +1,12 @@
+class Endpoints {
+  static String baseUrl = "https://api.example.com";
+  static String signIn = "user/signin";
+}
+
+class ApiKey {
+  static String status = "status"; 
+  static String errorMessage = "ErrorMessage"; 
+  static String email = "email"; 
+  static String password = "password"; 
+  static String token = "token"; 
+}
