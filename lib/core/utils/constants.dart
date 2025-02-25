6a0376053789185f4491d9f6e3188c9b668dca13
Mahmoud Beeko <mahmoud.m.bakir@gmail.com>
2025-02-25 18:56:31 +0200

    added my colors to constants

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,12 @@
+import 'package:flutter/material.dart';
+
 class Constants {
   static const String baseUrl = "https://api.themoviedb.org/3";
   static const String apiKey = "4e6ff8ecbc05d34b7129369aea810fc7";
+}
+
+class MyColors {
+  static const Color myGrey = Color(0x006f8fa0);
+  static const Color myBrown = Color(0x005a4b49);
+  static const Color myRed = Color(0x00b31a35);
 }
