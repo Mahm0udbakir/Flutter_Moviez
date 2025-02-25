25abaf6f70615b581535da22ea645b710473b9ac
Mahmoud Beeko <mahmoud.m.bakir@gmail.com>
2025-02-25 12:08:58 +0200

    added the api service in movie remote datasource

---
+++ lib/data/datasources/remote/movie_remote_datasource.dart
@@ -0,0 +1,19 @@
+import 'package:movie_app/core/api/dio_consumer.dart';
+import 'package:movie_app/data/models/movie_model.dart';
+
+class ApiService {
+  final DioConsumer dioConsumer;
+
+  ApiService({required this.dioConsumer});
+
+  Future<List<Movie>> getPopularMovies() async {
+    try {
+      final response = await dioConsumer.get('/movie/popular');
+      final List<dynamic> results = response['results'];
+      
+      return results.map((json) => Movie.fromJson(json)).toList();
+    } catch (e) {
+      throw Exception('Failed to load popular movies: $e');
+    }
+  }
+}
