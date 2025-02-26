3f955b001905c8ebdc748ade82bbb6036f9d7865
Mahmoud Beeko <mahmoud.m.bakir@gmail.com>
2025-02-26 11:33:11 +0200

    modified the try-catch block in popularmovieswebservices

--- lib/data/web_services/remote/popular_movie_web_services.dart
+++ lib/data/web_services/remote/popular_movie_web_services.dart
@@ -1,5 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:movie_app/core/api/dio_consumer.dart';
 import 'package:movie_app/core/api/endpoints.dart';
+import 'package:movie_app/core/error/exceptions.dart';
 import 'package:movie_app/data/models/popular_model.dart';
 
 class PopularMoviesWebServices {
@@ -13,6 +15,9 @@ class PopularMoviesWebServices {
       final List<dynamic> results = response['results'];
       return results.map((json) => PopularMoviesModel.fromJson(json)).toList();
     } catch (e) {
+      if (e is DioException) {
+        handleDioExceptions(e);
+      }
       throw Exception('Failed to load popular movies: $e');
     }
   }
